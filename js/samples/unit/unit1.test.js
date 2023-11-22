ea957b628f3a138f5463bbde4016b9bf7d8f24ad
Gareth Thomas <thomasgareth3@gmail.com>
2023-11-22 15:42:34 +0100

    premiers tests

---
+++ js/samples/unit/unit1.test.js
@@ -0,0 +1,7 @@
+import { sayHello } from "./unit1";
+
+describe("SayHello Unit Test Suites", () => {
+  it("should return Hello World", () => {
+    expect(sayHello()).toBe("Hello, World");
+  });
+});
